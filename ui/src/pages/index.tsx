07dce289915a2a5a0ec5e43c3b37698128ed0ca6
ahmadrosid <alahmadrosid@gmail.com>
2023-09-30 14:51:17 +0700

    feat: updating copy for landing page

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -6,24 +6,27 @@ import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <div className="container mx-auto pt-8">
-      <div className="max-w-lg pt-16 pb-8 [var(--primary:black)]">
-        <p>ReadClip</p>
+      <div className="max-w-2xl pt-16 pb-8 mx-auto text-center">
+        <p>Bookmark manager for organizing your links from internet.</p>
         <p className="mt-4 text-3xl font-extrabold tracking-[-0.04em] text-black sm:text-5xl sm:leading-[3.5rem] dark:text-white">
-          Bookmark manager for organizing your links from internet.
+          A better way to bookmark, read news, blogs, or any internet text
+          without ads.
         </p>
       </div>
-      <Link
-        to="/clip"
-        className={cn(
-          buttonVariants({
-            size: "lg",
-            className:
-              "items-center justify-between h-10 bg-black dark:bg-white text-white dark:text-black",
-          })
-        )}
-      >
-        Start bookmarking <ArrowRight className="w-4 h-4 ml-3" />
-      </Link>
+      <div className="text-center">
+        <Link
+          to="/clip"
+          className={cn(
+            buttonVariants({
+              size: "lg",
+              className:
+                "items-center justify-between h-10 bg-black dark:bg-white text-white dark:text-black",
+            })
+          )}
+        >
+          Start bookmarking <ArrowRight className="w-4 h-4 ml-3" />
+        </Link>
+      </div>
     </div>
   );
 }
