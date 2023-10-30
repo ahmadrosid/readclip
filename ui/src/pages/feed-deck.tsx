be60ce8efb131be7caeb2799db7e09f1134e847d
ahmadrosid <alahmadrosid@gmail.com>
2023-10-30 09:35:32 +0700

    feat: disable search feature in readclip feeddeck

--- ui/src/pages/feed-deck.tsx
+++ ui/src/pages/feed-deck.tsx
@@ -7,7 +7,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-import { PlusIcon, SearchIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { useCallback, useState } from "react";
@@ -141,14 +141,14 @@ export default function FeedDeckPage() {
                 <DialogHeader className="px-4 py-4 border-b bg-gray-50">
                   <DialogTitle>Add feed</DialogTitle>
                 </DialogHeader>
-                <div className="px-4 py-2 flex items-center gap-2">
+                {/*<div className="px-4 py-2 flex items-center gap-2">
                   <SearchIcon className="w-5 h-5 text-gray-500" />
                   <input
                     placeholder="Search"
                     className="p-2 w-full focus:outline-none"
                   />
-                </div>
-                <div className="max-h-[400px] overflow-y-auto border-t">
+              </div>*/}
+                <div className="max-h-[400px] overflow-y-auto">
                   <ul className="relative">
                     <FeedItem
                       type="github"
