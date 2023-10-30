04bea36462b92ba85200dbb134277f84a7c0075d
ahmadrosid <alahmadrosid@gmail.com>
2023-10-30 10:38:20 +0700

    feat: improve ux with cmdk component

--- ui/src/components/feed/feed-item-youtube.tsx
+++ ui/src/components/feed/feed-item-youtube.tsx
@@ -1,31 +1,31 @@
 import { useMutation } from "react-query";
-import { Input } from "@/components/ui/input";
 import { FindChannelResponse, findYoutubeChannels } from "@/lib/api/echotube";
 import { useDebouncedCallback } from "use-debounce";
 import { useState } from "react";
 import { XIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandLoading,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 export function FeedItemYoutube({
   onSubmit,
 }: {
   onSubmit: (channelId: string) => void;
 }) {
-  const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState<FindChannelResponse | undefined>();
   const findChannelMutation = useMutation({
     mutationFn: findYoutubeChannels,
     mutationKey: "search-youtube-channels",
-    onSuccess(data) {
-      if (data.length > 0) {
-        setOpen(true);
-      }
-    },
   });
 
   const debounced = useDebouncedCallback((value) => {
     if (value === "") {
-      setOpen(false);
       return;
     }
     findChannelMutation.mutate(value);
@@ -54,42 +54,42 @@ export function FeedItemYoutube({
             </Button>
           </div>
         ) : (
-          <label>
-            <span className="text-sm pb-2 block text-gray-600">
-              Search youtube channels
-            </span>
-            <Input
-              name="input_find_channels"
-              className="bg-white"
-              placeholder="MrBeast"
-              onChange={(e) => {
-                debounced(e.currentTarget.value);
-              }}
-            />
-          </label>
-        )}
-        {open && (
-          <div className="pt-1">
-            <ul className="bg-white border rounded-md overflow-hidden">
-              {findChannelMutation.data?.map((item) => (
-                <li
-                  onClick={() => {
-                    onSubmit(item.id);
-                    setSelected(item);
-                    setOpen(false);
-                  }}
-                  key={item.id}
-                  className="flex gap-3 p-1.5 hover:bg-gray-50 cursor-pointer"
-                >
-                  <img
-                    src={item.thumbnail}
-                    alt={item.name}
-                    className="rounded-full h-5 w-5"
-                  />
-                  <p>{item.name}</p>
-                </li>
-              ))}
-            </ul>
+          <div className="rounded-md overflow-hidden border">
+            <Command>
+              <CommandInput
+                onChangeCapture={(e) => debounced(e.currentTarget.value)}
+                placeholder="Search channels..."
+              />
+              <CommandList>
+                {findChannelMutation.isLoading && (
+                  <CommandLoading>
+                    <div className="flex justify-between p-2 text-sm text-gray-700">
+                      Loading...
+                    </div>
+                  </CommandLoading>
+                )}
+                <CommandGroup heading="Channels">
+                  {findChannelMutation.data?.map((item) => (
+                    <CommandItem
+                      value={item.name}
+                      onSelect={() => {
+                        onSubmit(item.id);
+                        setSelected(item);
+                      }}
+                      key={item.id}
+                      className="flex gap-3 p-1.5 hover:bg-gray-50 cursor-pointer"
+                    >
+                      <img
+                        src={item.thumbnail}
+                        alt={item.name}
+                        className="rounded-full h-5 w-5"
+                      />
+                      <p>{item.name}</p>
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
           </div>
         )}
       </div>
