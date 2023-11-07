320a3a92b5465475aab101ee6f54fe6948295927
ahmadrosid <alahmadrosid@gmail.com>
2023-11-07 20:56:11 +0700

    chore: tidy up

--- ui/src/pages/feed-deck.tsx
+++ ui/src/pages/feed-deck.tsx
@@ -149,13 +149,13 @@ export default function FeedDeckPage() {
                 <DialogHeader className="px-4 py-4 border-b bg-gray-50 dark:bg-gray-900">
                   <DialogTitle>Add feed</DialogTitle>
                 </DialogHeader>
-                {/*<div className="px-4 py-2 flex items-center gap-2">
+                {/* <div className="px-4 py-2 flex items-center gap-2">
                   <SearchIcon className="w-5 h-5 text-gray-500" />
                   <input
                     placeholder="Search"
                     className="p-2 w-full focus:outline-none"
                   />
-              </div>*/}
+                </div> */}
                 <div className="max-h-[400px] overflow-y-auto scrollbar-thin">
                   <ul className="relative">
                     <FeedItem
