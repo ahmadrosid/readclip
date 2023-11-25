05d6a0e95dc0c5b9e68be17ee01507336fbc2e66
ahmadrosid <alahmadrosid@gmail.com>
2023-11-25 16:25:15 +0700

    feat: add 404 page

---
+++ ui/src/pages/404.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="bg-gray-50">
+        <div className="bg-[url(http://res.cloudinary.com/dr15yjl8w/image/upload/v1700903715/public/bnbfhq50gw186716fo6a.png)] bg-[size:200px] h-screen w-full">
+          <div className="h-screen grid place-content-center">
+            <h1 className="text-[200px] font-black text-rose-500">404</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
