a2de2f1c7bd29a22f609947f43089f3900941cc3
ahmadrosid <alahmadrosid@gmail.com>
2024-04-04 14:28:09 +0700

    chore: tidy up

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Start redeploying..."
+
+# Stop and remove the existing container, if any
+if [ "$(docker ps -aq --filter "ancestor=readclip")" ]; then
+    docker stop $(docker ps -aq --filter "ancestor=readclip")
+    docker rm $(docker ps -aq --filter "ancestor=readclip")
+fi
+
+# Remove image from local registry
+echo "Removing ghcr.io/ahmadrosid/readclip:latest";
+docker rmi -f ghcr.io/ahmadrosid/readclip:latest
+
+docker compose up -f /root/readclip/docker-compose.yml -d
+
+echo "Rollout deployment done for version $VERSION_TAG"
