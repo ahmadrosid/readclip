ceb23b958d072a3c816cf69ae32d294952720932
ahmadrosid <alahmadrosid@gmail.com>
2024-05-11 22:57:02 +0700

    feat(ui): add collections page with coming soon message

--- ui/src/pages/collections/index.tsx
+++ ui/src/pages/collections/index.tsx
@@ -1,7 +1,8 @@
 export default function Collections() {
   return (
     <div className="p-4 md:p-8 min-h-[80vh]">
-        Welcome to collections
+        <h1 className="text-3xl font-bold">Collections <span className="text-gray-800/50">(Cooming soon)</span></h1>
+        <p className="py-4">Collection is the place where you can share your bookmarks to the world.</p>
     </div>
   );
 }
