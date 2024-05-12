7230d64e789e0d8506c41e9342df081d45dd473e
ahmadrosid <alahmadrosid@gmail.com>
2024-05-12 16:07:08 +0700

    feat: add llm platform together

--- copilot/ai_api_generator.py
+++ copilot/ai_api_generator.py
@@ -2,7 +2,7 @@ import os
 import sys
 from groq import Groq
 from utils import read_file_to_string
-from llm import ask_groq
+from llm import ask_groq, ask_together
 from prompt_toolkit import prompt as get_input
 
 client = Groq(
@@ -17,25 +17,28 @@ def reset_messages():
 
 def ask(question): 
     messages.append({"role": "user", "content": question})
-    response = ask_groq(messages)
+    # response = ask_groq(messages)
+    response = ask_together(messages)
     messages.append({"role": "assistant", "content": response})
 
 reset_messages()
 
-try:
-    while True:
-        user_input = get_input("Ask ai: ")
-        if user_input.lower() == 'exit' or user_input.lower() == 'q':
-            print("")
-            break
-        if user_input.lower() == 'reset':
-            reset_messages()
-            print("cleared...")
-            continue
-        if user_input == '':
-            continue
-        ask(question=user_input)
-except KeyboardInterrupt:
-    print("")
-except:
-    print("Unexpected error:", sys.exc_info()[0])
+ask(question="create api to store notes, the notes will be belong for some url.")
+
+# try:
+#     while True:
+#         user_input = get_input("Ask ai: ")
+#         if user_input.lower() == 'exit' or user_input.lower() == 'q':
+#             print("")
+#             break
+#         if user_input.lower() == 'reset':
+#             reset_messages()
+#             print("cleared...")
+#             continue
+#         if user_input == '':
+#             continue
+#         ask(question=user_input)
+# except KeyboardInterrupt:
+#     print("")
+# except:
+#     print("Unexpected error:", sys.exc_info()[0])
--- copilot/llm.py
+++ copilot/llm.py
@@ -1,18 +1,37 @@
 import os
 from groq import Groq
+from together import Together
 
-client = Groq(
+groq_client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
+together_client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
+
 def ask_groq(messages):
-    chat_completion = client.chat.completions.create(
+    chat_completion = groq_client.chat.completions.create(
         messages=messages,
         model="llama3-70b-8192",
         stream=True,
     )
     result = ""
     for chunk in chat_completion:
+        result += chunk.choices[0].delta.content
+        print(chunk.choices[0].delta.content, end="", flush=True)
+    print("\n")
+    return result
+
+def ask_together(messages):
+    response = together_client.chat.completions.create(
+        messages=messages,
+        # model="meta-llama/Llama-3-8b-chat-hf",
+        # model="codellama/CodeLlama-13b-Instruct-hf",
+        # model="codellama/CodeLlama-7b-Instruct-hf",
+        model="mistralai/Mixtral-8x22B-Instruct-v0.1",
+        stream=True,
+    )
+    result = ""
+    for chunk in response:
         result += chunk.choices[0].delta.content
         print(chunk.choices[0].delta.content, end="", flush=True)
     print("\n")
